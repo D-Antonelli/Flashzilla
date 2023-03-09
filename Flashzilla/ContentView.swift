01929cdfbb9b0f0cbd87eec61a63e99a7d5e8374
derya <derya@boonandmoil.dev>
2023-03-09 09:40:03 +0000

    designing a single card view

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,29 +9,10 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
-    @State private var scale = 1.0
     
     var body: some View {
-        Text("Hello, World!")
-            .scaleEffect(scale)
-            .onTapGesture {
-                withOptionalAnimation {
-                    scale *= 1.5
-                }
-            }
+        CardView(card: Card.example)
     }
-    
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
-        }
-    }
-    
-    
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
